c451ace19a71833158090846b5de2e50eb61aa41
Adrián Villalba <a.villalba.2023@alumnos.urjc.es>
2026-05-03 09:26:06 +0200

    Base64 fixed

--- 1. Criptografia/1.2. Base64.py
+++ 1. Criptografia/1.2. Base64.py
@@ -1,7 +1,13 @@
 import base64
 textoCodificado =("VVJKQ3tTTTRMTF9CNFMzXzY0fQ==")
+textoSinCodificar = ("hola")
 
 decodificado = base64.b64decode(textoCodificado)
+datos_en_bytes = textoSinCodificar.encode("UTF-8")
+
+codificado = base64.b64encode(datos_en_bytes)
+texto_codificado = codificado.decode("UTF-8")
 
 print(decodificado)
 print(decodificado.decode("UTF-8"))
+print(texto_codificado)
